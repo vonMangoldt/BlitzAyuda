c889d03f4aff21c74b1f34c7ff179cf3fc64867e
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2025-11-22 11:57:00 -0300

    rename and reassign contracts for std script

--- script/CallDataVerifyer.s.sol
+++ script/CallDataVerifyer.s.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {CallDataVerifyer} from "../src/CallDataVerifyer.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract CallDataVerifyerScript is Script {
+    CallDataVerifyer public callDataVerifyer;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        callDataVerifyer = new CallDataVerifyer();
 
         vm.stopBroadcast();
     }
