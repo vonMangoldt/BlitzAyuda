c43865286076d3a40b533e8334ce0083052c3c12
vonMangoldt <61846574+vonMangoldt@users.noreply.github.com>
2025-11-22 11:32:06 -0300

    add CalldataVerifyer

---
+++ src/CallDataVerifyer.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract CallDataVerifyer {
+    function verifyCallData(
+        bytes memory callData
+    )
+        public
+        pure
+        returns (bool)
+    {
+        return true;
+    }
+
+    function _checkSelector(
+        bytes memory _callData
+    )
+        internal
+        pure
+        returns (bytes4 selector)
+    {
+        assembly {
+            selector := mload(add(_callData, 32))
+        }
+    }
+}
